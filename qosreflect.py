523fe8aa75eb74d37ca38154a9e36a86b49d04ed
phonethihakyaw <phonethihakyaw5@gmail.com>
2019-02-25 04:17:11 +0630

    Update qosreflect.py

--- qosreflect.py
+++ qosreflect.py
@@ -1,6 +1,4 @@
 #!/usr/bin/env python
-#Copyright (c) 2014, Paessler AG <support@paessler.com>
-#All rights reserved.
 #Redistribution and use in source and binary forms, with or without modification, are permitted provided that the
 # following conditions are met:
 #1. Redistributions of source code must retain the above copyright notice, this list of conditions
